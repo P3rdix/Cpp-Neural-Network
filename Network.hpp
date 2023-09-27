562cf36892063ca221042ec01ebd0c74edba5148
Vedant Nair <vedantxiii@gmail.com>
2023-09-28 00:35:57 +0530

    Layout of Network Class created

--- Network.hpp
+++ Network.hpp
@@ -1,5 +1,61 @@
 #ifndef NETWORK
 #define NETWORK
+#include <string>
+#include <iostream>
 
+struct data{
+    float* Output_Values;
+    int size;
+};
+
+class Node;
+
+class Layer;
+
+class Network{
+    private:
+        int num_layers;
+        Layer* Start;
+        int forward_propogate(float**, int, int, struct data*);
+        int back_propogate();
+    public:
+        int Add_Layer(int, std::string);
+        int train(float**, float**, int, int, int); 
+        int test(float**, float**, std::string);
+};
+
+int Network::Add_Layer(int num_of_Nodes, std::string activation_fn){
+    return 0;
+}
+
+int Network::train(float** x, float** y, int length, int epochs, int batch_size){
+    if(num_layers == 0){
+        std::cerr<<"The network is incomplete. Please Add layers to it before running";
+        return -1;
+    }
+    for(int i=0;i<length/batch_size;i++){
+        struct data *output = (struct data*)malloc(sizeof(struct data));
+        forward_propogate(x, batch_size*i, (i == length/batch_size - 1)?(length%batch_size):(batch_size) );
+        back_propogate();
+    }
+    
+    return 0;
+}
+
+int Network::test(float** x, float** y, std::string metric){
+    return 0;
+}
+
+int Network::forward_propogate(float** data, int base, int batch_size, struct data* out){
+
+    for(int i=base;i<base+batch_size;i++){
+        std::cout<<"Hello";
+    }
+    return 0;
+}
+
+int Network::back_propogate(){
+    return 0;
+}
 
 #endif
