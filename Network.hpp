469715a5b3b18d7f4609b4a1b3c217cbda32fbda
Vedant Nair <vedantxiii@gmail.com>
2023-10-09 01:07:26 +0530

    Layer addition mechanism added, along with Constructor.

--- Network.hpp
+++ Network.hpp
@@ -1,9 +1,8 @@
 #ifndef NETWORK
 #define NETWORK
-#include <string>
-#include <iostream>
-#include "Node.hpp"
+
 #include "Layer.hpp"
+#include <exception>
 
 struct data{
     float* Output_Values;
@@ -12,22 +11,65 @@ struct data{
 
 class Network{
     private:
-        int num_layers;
-        Layer* Start;
+        int no_layers;
+        std::vector<Layer*> layers;
         int forward_propogate(float**, int, int, struct data*);
         int back_propogate();
     public:
         int Add_Layer(int, std::string);
+        int Add_Layer(int, int, int*, std::string);
+        int Add_Input_Layer(int, int*);
         int train(float**, float**, int, int, int); 
         int test(float**, float**, std::string);
+        Network();
 };
 
+Network::Network(){
+    no_layers = 0;
+}
+
 int Network::Add_Layer(int num_of_Nodes, std::string activation_fn){
+    if(!no_layers){
+        return 1;
+    }
+    else{
+        try{
+            layers.emplace_back(new Layer(num_of_Nodes,layers[no_layers-1]->layer_length,activation_fn));
+        } catch(Unknown_Activation_Function_Exception e){
+            return 1;
+        }
+    }
     return 0;
 }
 
+int Network::Add_Input_Layer(int input_dim, int* input_size){
+    if(input_dim == 1){
+        layers.emplace_back(new Layer(*(input_size),1,"identity"));
+    }
+    else if(input_dim == 2){
+        layers.emplace_back(new Layer(*(input_size), *(input_size+1),"identity"));
+    }
+    else{
+        return 1;
+    }
+    return 0;
+}
+
+int Network::Add_Layer(int no_nodes, int input_dim, int* input_size, std::string activation_function){
+    if(no_layers){
+        int r = this->Add_Layer(no_nodes, activation_function);
+        return r;
+    }
+    int a = this->Add_Input_Layer(input_dim, input_size);
+    if(a){
+        a = this->Add_Layer(no_nodes, activation_function);
+        return a;
+    }
+
+}
+
 int Network::train(float** x, float** y, int length, int epochs, int batch_size){
-    if(num_layers == 0){
+    if(no_layers == 0){
         std::cerr<<"The network is incomplete. Please Add layers to it before running\n";
         return -1;
     }
